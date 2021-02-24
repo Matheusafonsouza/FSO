8c853b93ac44d2b3bdadde61e4e9bba477c7e21a
matheusafonsouza <matheusafonsouza@gmail.com>
2021-02-24 12:43:39 -0300

    fix: exercise C

---
+++ lista1/c.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <stdlib.h>
+
+char name[100];
+
+void readName()
+{
+  int scanfReturn;
+
+  scanfReturn = scanf(" %s", name);
+  if (scanfReturn == EOF)
+  {
+    exit(0);
+  }
+}
+
+void handleTerm()
+{
+  readName();
+  if (strcmp(name, "Con2") == 0)
+  {
+    puts("Certo");
+  }
+  else
+  {
+    kill(getppid(), SIGALRM);
+    puts("Erro");
+  }
+}
+
+void handleUsr1()
+{
+  readName();
+  if (strcmp(name, "Igor3k") == 0)
+  {
+    puts("Certo");
+  }
+  else
+  {
+    kill(getppid(), SIGALRM);
+    puts("Erro");
+  }
+}
+
+void handleUsr2()
+{
+  readName();
+  if (strcmp(name, "Monark") == 0)
+  {
+    puts("Certo");
+  }
+  else
+  {
+    kill(getppid(), SIGALRM);
+    puts("Erro");
+  }
+}
+
+void handleAlrm()
+{
+  readName();
+  if (strcmp(name, "Silencio") == 0)
+  {
+    puts("Certo");
+  }
+  else
+  {
+    kill(getppid(), SIGALRM);
+    puts("Erro");
+  }
+}
+
+void handleInt()
+{
+  readName();
+  if (strcmp(name, "Con1") == 0)
+  {
+    puts("Certo");
+  }
+  else
+  {
+    kill(getppid(), SIGALRM);
+    puts("Erro");
+  }
+}
+
+int main()
+{
+  signal(SIGTERM, handleTerm);
+  signal(SIGUSR1, handleUsr1);
+  signal(SIGUSR2, handleUsr2);
+  signal(SIGINT, handleInt);
+  signal(SIGALRM, handleAlrm);
+  while (1)
+  {
+    pause();
+  }
+}
